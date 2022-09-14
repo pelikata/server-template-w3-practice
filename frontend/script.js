e037ee882abeb08a2e24eba91ae10523465549f7
Joe <pelikata@gmail.com>
2022-09-14 12:06:21 +0200

    js-practice

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,31 @@
 
 let myVariable = "value";
-console.log(myVariable);
+// console.log(myVariable);
 myVariable= "new value";
-console.log(myVariable);
+// console.log(myVariable);
 myVariable= 2;
-console.log(myVariable);
+// console.log(myVariable);
 myVariable = true;
-console.log(myVariable);
+// console.log(myVariable);
 
 let myVariable2 = "1987";
-console.log(myVariable2);
+// console.log(myVariable2);
 
 myVariable = myVariable2;
-console.log(myVariable);
+// console.log(myVariable);
 
 let myVariable3 = myVariable + myVariable2;
-console.log (myVariable3);
+// console.log (myVariable3);
+
+function sumVariables(variable1, variable2) {
+    console.log(variable1 + variable2);
+}
+sumVariables(myVariable, myVariable2)
+sumVariables(8, "nyolc");
+sumVariables("word1", "word2");
+
+function loadEvent() {
+    console.log("loaded");
+}
+
+window.addEventListener("click", loadEvent);
