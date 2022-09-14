90a9238cc965f08f04491b8f2f1eb31d435d9708
Joe <pelikata@gmail.com>
2022-09-14 11:43:05 +0200

    variables

---
+++ frontend/script.js
@@ -0,0 +1,18 @@
+
+let myVariable = "value";
+console.log(myVariable);
+myVariable= "new value";
+console.log(myVariable);
+myVariable= 2;
+console.log(myVariable);
+myVariable = true;
+console.log(myVariable);
+
+let myVariable2 = "1987";
+console.log(myVariable2);
+
+myVariable = myVariable2;
+console.log(myVariable);
+
+let myVariable3 = myVariable + myVariable2;
+console.log (myVariable3);
